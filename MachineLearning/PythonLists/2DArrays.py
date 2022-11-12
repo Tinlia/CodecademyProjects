6c7abf548aa8ee9f1cab8f8fcb309b2494299a3c
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2022-11-12 12:48:36 -0500

    Create 2DArrays.py

---
+++ MachineLearning/PythonLists/2DArrays.py
@@ -0,0 +1,9 @@
+heights = [["Jenny", 61], ["Alexus", 70], ["Sam", 67], ["Grace", 64], ["Vik", 68]]
+
+ages = [["Aaron", 15], ["Dhruti",16]]
+
+tictactoe = [
+  ["X","O","X"],
+  ["O","X","O"],
+  ["o","O","X"]
+]
