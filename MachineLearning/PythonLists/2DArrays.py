a353ad515e62d1b9d38ddad9ca94a8f4a67ccb76
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2022-11-12 13:09:19 -0500

    Update 2DArrays.py

--- MachineLearning/PythonLists/2DArrays.py
+++ MachineLearning/PythonLists/2DArrays.py
@@ -25,3 +25,19 @@ print(sams_score)
 
 ellies_score = class_name_test[-1][-1]
 print(ellies_score)
+
+#Incoming students, their nationalities, and their grade
+incoming_class = [
+  ["Kenny", "American", 9],
+  ['Tanya', "Russian", 9],
+  ["Madison", "Indian", 7]
+]
+print(incoming_class)
+
+  #Move Madison from grade 7 to grade 8
+incoming_class[2][2] = 8
+print(incoming_class)
+
+  #Change Kenny to Ken using only negative indices
+incoming_class[-3][-3] = "Ken"
+print(incoming_class)
