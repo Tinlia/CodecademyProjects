f9494e092a1a9e029353339277a5714af83db739
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2022-11-12 20:53:56 -0500

    Create ListInsertion.py

---
+++ MachineLearning/PythonLists/ListInsertion.py
@@ -0,0 +1,7 @@
+front_display_list = ["Mango", "Filet Mignon", "Chocolate Milk"]
+print(front_display_list)
+
+# Add "Pineapple" to the start of the list
+front_display_list.insert(0,"Pineapple")
+
+print(front_display_list)
