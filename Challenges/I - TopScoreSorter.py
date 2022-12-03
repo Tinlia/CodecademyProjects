37d86a6ec69b89e03cb66db778d587008d532e62
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2022-12-03 02:24:37 -0500

    Create I - TopScoreSorter.py

---
+++ Challenges/I - TopScoreSorter.py
@@ -0,0 +1,19 @@
+# Evan "Tinlia" Kimpton
+# Create a program that sorts scores from greatest to lowest
+# Note: This project must be done without the sort() method
+def score_sorter(a):
+    length = len(a)
+    hold = 0
+    while True:
+        check = True
+        for i in range(0, length - 1):
+            if a[i] < a[i + 1]:
+                hold = a[i]
+                a[i] = a[i + 1]
+                a[i + 1] = hold
+                check = False
+        if check: return a
+
+score_list = [1, 2, 3, 9999, 13]
+
+print(score_sorter(score_list))
