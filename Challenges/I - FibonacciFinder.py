408e9d2dc274eb792ad1a7b31da4f30cb5176abb
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2022-12-18 14:33:02 -0500

    Update I - FibonacciFinder.py

--- Challenges/I - FibonacciFinder.py
+++ Challenges/I - FibonacciFinder.py
@@ -2,11 +2,11 @@
 # Create a program that takes a number and prints the corresponding Fibonacci number.
 # Assume n=1 returns 0 and n=2 returns 1
 def fib_finder(n):
-  a, b, total = 0, 1, 0
-  if n == 2: return 1
-  for i in range(1,n-1):
-    total = a + b
-    a, b = b, total
+  a, b, total = 0, 1, 0   # The first two numbers of the Fibonacci order are 0,1
+  if n == 2: return 1     # Base Case
+  for i in range(1,n-1):  
+    total = a + b         # Acts as both a hold value and the total
+    a, b = b, total       # Shift values left i.e., 0+1=1 -> 1+1=2 -> 1+2=3
   return total
 
 print(fib_finder(10))
