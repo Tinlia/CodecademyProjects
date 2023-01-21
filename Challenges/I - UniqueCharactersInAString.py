9a339ff8d0221661ec8b4e6b975298a3c7e5d4af
Evan Kimpton <65005430+Tinlia@users.noreply.github.com>
2023-01-21 14:26:28 -0500

    Update I - UniqueCharactersInAString.py

--- Challenges/I - UniqueCharactersInAString.py
+++ Challenges/I - UniqueCharactersInAString.py
@@ -2,19 +2,15 @@
 # Create a program that takes in a String and returns if all the characters are unique or not
 def unique_characters(string_in):
     if string_in == "":
-      return "Error, invalid input"
+        return "Error, invalid input"
     a = []
-    if not string_in: return True
-    letList = list(string_in.lower())
-    for i in letList:
+    lettList = list(string_in.lower())
+    for i in lettList:
         check = True
         for j in a:
-            if i == j:
-                check = False
-        if check:
-            a.append(i)
-        else:
-            return False
+            if i == j: check = False
+        if check: a.append(i)
+        else: return False
     return True
 
 print(unique_characters("Apple"))
