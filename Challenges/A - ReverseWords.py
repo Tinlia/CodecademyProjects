2892eba893f023c78c0d2f17db0c0bff856dc2c1
unknown <nm20eb@brocku.ca>
2024-02-06 00:48:14 -0500

    Optimize Code

--- Challenges/A - ReverseWords.py
+++ Challenges/A - ReverseWords.py
@@ -1,18 +1,8 @@
 # Evan "Tinlia" Kimpton
 # Given an input phrase, reverse the word order but not the letter order
 # Input: "My Name Is Evan" Output: "Evan Is Name My"
+
 def word_reverser(phrase):
-    word, reverse, hold = "", "", []
-    for x in phrase:                  # Turns the string into a char array without reinitializing
-        if x != ' ': word += x        
-        else:                         # When the end of a word is found, save the word to an array of words
-            hold.append(word)
-            hold.append(" ")          # Add a space
-            word = ""
-    hold.append(word)                 # Add the last word, no space
-    hold.reverse()                    # Reverse the word order
-    for x in hold: reverse += x       # Convert array to string
-    return reverse
+    return ' '.join(phrase.split()[::-1])
 
-
-print(word_reverser('Hello Worlds'))
+print(word_reverser('Hello Worlds')) # Worlds Hello
