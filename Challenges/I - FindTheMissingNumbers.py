6252d576932bf202d919c7a34e651a82a8c5d310
unknown <nm20eb@brocku.ca>
2024-02-06 01:13:17 -0500

    Optimize Code

--- Challenges/I - FindTheMissingNumbers.py
+++ Challenges/I - FindTheMissingNumbers.py
@@ -1,15 +1,10 @@
 # Evan "Tinlia" Kimpton
 # Create a program that takes a list of numbers and returns the missing numbers in a list
 def missing_nos(arr, k):
-  a=[]
-  i, j = 0, 0
-  while i < len(arr):
-    if j != arr[i]:
-      a.append(arr[i]-1)
-      j = j+2
-    else : j= j + 1
-    i= i+1
-  a.remove(0)
-  return a
+  missing = []
+  for i in range(0, len(arr) - 1):
+    if arr[i] + 1 != arr[i+1]:
+      missing.append(arr[i] + 1)    
+  return missing
 
-print(missing_nos([1, 2, 4, 5, 6, 7, 8, 10], 2))
+print(missing_nos([1, 2, 4, 5, 6, 7, 8, 10], 2)) # [3, 9]
