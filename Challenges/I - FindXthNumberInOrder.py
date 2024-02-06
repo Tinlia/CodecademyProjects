564f7813dcb91c0b3673be1296649acfe17b002e
unknown <nm20eb@brocku.ca>
2024-02-05 21:25:41 -0500

    Optimize Solution (14 -> 7)

--- Challenges/I - FindXthNumberInOrder.py
+++ Challenges/I - FindXthNumberInOrder.py
@@ -1,13 +1,7 @@
 # Evan "Tinlia" Kimpton
 # Given an array of numbers, return the xth number in order
 def getX(x, nums):
-    # Parameter checks
-    if x > len(nums): return 0
-    if not nums: return 0
-    # Sort the array
     nums.sort()
-    # Find the xth number
-    for i in range (len(nums)+1):
-        if i == x-1: return nums[i]
+    return 0 if (x > len(nums) or x<1) else nums[x-1]
 
-print(getX(2, [6, 3, -1, 5]))
+print(getX(2, [6, 3, -1, 5])) # 3
